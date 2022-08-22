57f19fc7d0fe188b9930d5b2f84bd5978997ccae
plalgiush <ilyasushko@yandex.ru>
2022-08-22 18:56:44 +0300

    final version of the task 3.1

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let persons = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -28,31 +30,31 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     console.log(id)
-    const note = notes.find(note => note.id === id)
-    console.log(note)
-    if (note) {
-        response.json(note)
+    const person = persons.find(person => person.id === id)
+    console.log(person)
+    if (person) {
+        response.json(person)
     } else {
-        response.statusMessage = "Current note does not match";
+        response.statusMessage = "Current person does not match";
         response.status(404).end();
     }
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
         : 0
     return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if (!body.content) {
@@ -61,21 +63,21 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const person = {
         content: body.content,
         important: body.important || false,
         date: new Date(),
         id: generateId(),
     }
 
-    notes = notes.concat(note)
+    notes = notes.concat(person)
 
-    response.json(note)
+    response.json(person)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
