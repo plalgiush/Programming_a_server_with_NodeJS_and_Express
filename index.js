3e2a11d4cd4b0d4bc5932e2e7ac573728e5b6b5c
plalgiush <ilyasushko@yandex.ru>
2022-11-15 12:46:07 +0300

    finaly version of the tasks 3.15-3.18

--- index.js
+++ index.js
@@ -34,30 +34,6 @@ const Person = mongoose.model('Person', personSchema)
 
 app.use(cors())
 app.use(express.static('build'))
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "phone": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "phone": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "phone": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "phone": "39-23-6423122"
-    }
-]
-
 app.use(express.json())
 
 morgan.token('body', (request) => JSON.stringify(request.body))
@@ -75,44 +51,32 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/info', (request, response) => {
-    const qty = Number(persons.length)
-    process.env.TZ = 'Europe/Amsterdam'
-    const time = new Date().toString()
+    Person.find({}).then((persons) => {
+        const qty = Number(persons.length)
+        process.env.TZ = 'Europe/Amsterdam'
+         const time = new Date().toString()
 
-    response.send(`
-        <p>Phonebook has info for ${qty} people</p>
-        <p>${time}</p>
-    `)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    // const id = Number(request.params.id)
-    // console.log(id)
-    // const person = persons.find(person => person.id === id)
-
-    PersonModel.findById(request.params.id).then(person => {
-        response.json(person)
+        response.send(`
+            <p>Phonebook has info for ${qty} people</p>
+            <p>${time}</p>
+        `)
     })
-
-    // console.log(person)
-    // if (person) {
-    //     response.json(person)
-    // } else {
-    //     response.statusMessage = "Current person does not match";
-    //     response.status(404).end();
-    // }
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
-}
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(persons => {
+            if (persons) {
+                response.json(persons)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error), console.log(persons))
+})
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    const name = persons.filter(person => person.name === body.name).map(name => name.name).join()
     
     if (!body.name || !body.phone) {
         return response.status(400).json({
@@ -120,29 +84,37 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (name === body.name) {
-        return response.status(400).json({
-            error: 'name is busy'
-        })
-    }
-
     const person = new Person ({
-        id: generateId(),
         name: body.name,
         phone: body.phone,
     })
 
-    return person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    return person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
